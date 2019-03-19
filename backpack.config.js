87d838fd282c3bac51f0f41947ba5299687b1952
Wahyu Nugraha <nugraha.c.wahyu@gmail.com>
2019-03-19 21:21:21 +0700

    update default output dir

--- backpack.config.js
+++ backpack.config.js
@@ -3,6 +3,7 @@ const webpackConfig = require('./webpack.config')
 
 module.exports = {
   webpack: (config, options, webpack) => {
+    config.output.path = resolve(__dirname, 'dist')
     return Object.assign(config, webpackConfig)
   }
 }
