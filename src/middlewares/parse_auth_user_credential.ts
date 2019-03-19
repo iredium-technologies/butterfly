17f51b562ccb616460aebd38b837f90203627de0
Wahyu Nugraha <nugraha.c.wahyu@gmail.com>
2019-03-19 18:27:07 +0700

    assign user object to req

--- src/middlewares/parse_auth_user_credential.ts
+++ src/middlewares/parse_auth_user_credential.ts
@@ -1,7 +1,7 @@
-// import { UserService } from '~/src/services/user'
+import { UserService } from '~/src/services/user'
 import { BaseMiddleware } from '~/src/middlewares/base_middleware'
 
-// const User = new UserService()
+const User = new UserService()
 
 export class ParseAuthUserMiddleware extends BaseMiddleware {
   public static default (): Function {
@@ -10,7 +10,7 @@ export class ParseAuthUserMiddleware extends BaseMiddleware {
       const scopes = scopesString ? scopesString.split(' ') : []
       const userId = req.get('X-Authenticated-Userid')
       let user = null
-      // if (userId) user = await User.get(userId)
+      if (userId) user = await User.get(userId)
       if (userId) user = null
       req.user = user
       req.authInfo = { scopes }
