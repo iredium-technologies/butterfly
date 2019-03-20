76491d2aa530614e5445d98703c33cb4fe1b219d
Wahyu Nugraha <nugraha.c.wahyu@gmail.com>
2019-03-20 22:49:56 +0700

    set model type

--- src/services/user.ts
+++ src/services/user.ts
@@ -1,7 +1,11 @@
 import { User as Model } from '~/src/models/user'
 import { BaseService } from '~/src/services/base_service'
+import { UserInterface } from '~/src/models/user/interface'
+import mongoose = require('mongoose')
 
 export class UserService extends BaseService {
+  public Model: mongoose.Model<UserInterface>
+
   public constructor () {
     super(Model)
   }
