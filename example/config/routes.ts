7ba9134530fe163d5d2854bf1fb1d288a62018b5
Wahyu Nugraha <nugraha.c.wahyu@gmail.com>
2019-03-21 00:33:50 +0700

    add me route

--- example/config/routes.ts
+++ example/config/routes.ts
@@ -1,5 +1,7 @@
 import * as c from '~/example/controllers'
+import { handle } from '~/src/routes/handle'
 
 export function routes (route): void {
+  route.get('/me', handle(c.UsersController, 'me'))
   route.resources('/users', c.UsersController)
 }
