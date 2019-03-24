51872a8962921b82112c614a52f2418306a8d521
Wahyu Nugraha <nugraha.c.wahyu@gmail.com>
2019-03-24 14:10:06 +0700

    add tasks route test

--- test/routes.test.ts
+++ test/routes.test.ts
@@ -2,10 +2,19 @@ import Butterfly from '~/src/butterfly'
 import config from '~/example/config'
 import request = require('supertest')
 
-describe('GET /users', (): void => {
+const butterfly = new Butterfly(config)
+butterfly.boot()
+
+describe('Routes Test', (): void => {
   it('should respond ok', async (): Promise<void> => {
-    const butterfly = new Butterfly(config)
-    butterfly.boot()
     await request(butterfly.app).get('/users').expect(200)
   })
 })
+
+describe('GET /tasks', (): void => {
+  it('should respond ok', async (): Promise<void> => {
+    await request(butterfly.app).get('/tasks').expect(200)
+  })
+})
+
+butterfly.close()
