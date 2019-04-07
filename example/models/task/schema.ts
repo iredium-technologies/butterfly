0729cd9fd8ee1a12eaad388ade88f9524b38884a
Wahyu Nugraha <nugraha.c.wahyu@gmail.com>
2019-04-07 23:11:07 +0700

    update default user model name

--- example/models/task/schema.ts
+++ example/models/task/schema.ts
@@ -2,7 +2,7 @@ import { BaseSchema } from '~/src/models/base_schema'
 import mongoose = require('mongoose')
 
 export var Schema = new BaseSchema({
-  user: { type: mongoose.Schema.Types.ObjectId, ref: '$User', required: true, protect: true, autopopulate: { select: 'id username first_name last_name default_address email' } },
+  user: { type: mongoose.Schema.Types.ObjectId, ref: '_User', required: true, protect: true, autopopulate: { select: 'id username first_name last_name default_address email' } },
   title: { type: String, required: true },
   description: { type: String, required: false },
   complete: { type: Boolean, default: false }
