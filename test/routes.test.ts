57612eeaa83758c5124b51f3098b7b3d297e3c55
Wahyu Nugraha <nugraha.c.wahyu@gmail.com>
2019-04-09 01:37:15 +0700

    fix: async test

--- test/routes.test.ts
+++ test/routes.test.ts
@@ -2,17 +2,19 @@ import Butterfly from '~/src/butterfly';
 import config from '~/example/config'
 import request = require('supertest')
 
-const butterfly = new Butterfly(config)
-butterfly.boot()
-
 describe('Routes Test', (): void => {
   it('should respond ok for GET /users', async (): Promise<void> => {
-    await request(butterfly.app).get('/users').expect(200)
+    const butterfly = new Butterfly(config)
+    await butterfly.boot()
+    request(butterfly.app).get('/users').expect(200)
+    await butterfly.close()
   })
+
 
   it('should respond ok for GET /task', async (): Promise<void> => {
-    await request(butterfly.app).get('/tasks').expect(200)
+    const butterfly = new Butterfly(config)
+    await butterfly.boot()
+    request(butterfly.app).get('/tasks').expect(200)
+    await butterfly.close()
   })
 })
-
-butterfly.close()
