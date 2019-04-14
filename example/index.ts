a2b639f94c96420778ba24dee0ca9bf467dac587
Wahyu Nugraha <nugraha.c.wahyu@gmail.com>
2019-04-14 12:35:42 +0700

    use ready hook

--- example/index.ts
+++ example/index.ts
@@ -3,4 +3,8 @@ import config from '~/example/config'
 
 const butterfly = new Butterfly(config)
 
+butterfly.hook('butterfly:ready', ({ port }): void => {
+  console.log(`butterfly is listening on port ${port}`)
+})
+
 butterfly.boot()
