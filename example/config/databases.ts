0eaaef58e276ec106fb49a9927ead2caedd8e359
Wahyu Nugraha <nugraha.c.wahyu@gmail.com>
2019-04-14 12:33:50 +0700

    enable mongo

--- example/config/databases.ts
+++ example/config/databases.ts
@@ -1,6 +1,6 @@
 export const databases = (): object => ({
   mongo: {
-    enable: false,
+    enable: true,
     host: process.env.MONGO_HOST,
     port: process.env.MONGO_PORT,
     database: process.env.MONGO_DATABASE,
