cd6f1b8e2edb78e72b10edf57b8e5eafab17973d
Wahyu Nugraha <nugraha.c.wahyu@gmail.com>
2019-04-26 14:00:05 +0700

    remove console.log

--- src/helpers/compare_hash.ts
+++ src/helpers/compare_hash.ts
@@ -2,7 +2,6 @@ import bcrypt = require('bcrypt')
 
 export function compareHash (plainString, hashedString): Promise<boolean> {
   return new Promise((resolve, reject): void => {
-    console.log({plainString, pass: hashedString})
     bcrypt.compare(plainString, hashedString, (err, isMatch): void => {
       if (err) {
         reject(isMatch)
