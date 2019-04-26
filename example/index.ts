cb16b939e1a0abcaf02f21aaf9dbdf3576d97e57
Wahyu Nugraha <nugraha.c.wahyu@gmail.com>
2019-04-26 09:07:57 +0700

    log error

--- example/index.ts
+++ example/index.ts
@@ -7,4 +7,8 @@ butterfly.hook('butterfly:ready', ({ port }): void => {
   console.log(`butterfly is listening on port ${port}`)
 })
 
+butterfly.hook('butterfly:onError', (error): void => {
+  console.error(error)
+})
+
 butterfly.boot()
