5481222453c894ae164f803b85282f04a1a16791
Wahyu Nugraha <nugraha.c.wahyu@gmail.com>
2019-04-27 19:16:19 +0700

    add empty object as default paramater

--- src/middlewares/base_middleware.ts
+++ src/middlewares/base_middleware.ts
@@ -3,16 +3,16 @@ import { Class } from '~/src/types/class';
 import { UserService } from '~/src/services'
 
 export abstract class BaseMiddleware {
-  public abstract generate ({ databases }): express.RequestHandler
+  public abstract generate (ctx: object): express.RequestHandler
   protected userServiceClass: Class
 
   public constructor (userServiceClass: Class = UserService) {
     this.userServiceClass = userServiceClass
   }
 
-  public handleMiddelware ({ databases }): express.RequestHandler {
+  public handleMiddelware (ctx = {}): express.RequestHandler {
     return (req, res, next): void => {
-      const middleware = this.generate({ databases })
+      const middleware = this.generate(ctx)
       middleware(req, res, next)
         .catch((e) => {
           next(e)
