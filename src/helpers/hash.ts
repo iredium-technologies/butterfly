0caa943b6be914e2b205a847633c98bc14d0dde4
Wahyu Nugraha <nugraha.c.wahyu@gmail.com>
2019-05-01 11:39:37 +0700

    add hash helper

---
+++ src/helpers/hash.ts
@@ -0,0 +1,35 @@
+import bcrypt = require('bcrypt')
+
+const SALT_WORK_FACTOR = 10
+
+function hashCallback (resolve, reject): Function {
+  return function (err, hash): void {
+    if (err) return reject(err)
+    resolve(hash)
+  }
+}
+
+function genSaltCallback (plainString, resolve, reject): Function {
+  return function (err, salt): void {
+    if (err) return reject(err)
+    bcrypt.hash(plainString, salt, hashCallback(resolve, reject))
+  }
+}
+
+export const hashString = (plainString): Promise<string> => {
+  return new Promise((resolve, reject): void => {
+    bcrypt.genSalt(SALT_WORK_FACTOR, genSaltCallback(plainString, resolve, reject))
+  })
+}
+
+export function compareHash (plainString, hashedString): Promise<boolean> {
+  return new Promise((resolve, reject): void => {
+    bcrypt.compare(plainString, hashedString, (err, isMatch): void => {
+      if (err) {
+        reject(isMatch)
+      } else {
+        resolve(isMatch)
+      }
+    })
+  })
+}
