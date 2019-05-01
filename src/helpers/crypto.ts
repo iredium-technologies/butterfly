7c4f056f46b5ae16f6c9de2c8c2ede83940a2fb0
Wahyu Nugraha <nugraha.c.wahyu@gmail.com>
2019-05-01 12:37:24 +0700

    add encoding argument

--- src/helpers/crypto.ts
+++ src/helpers/crypto.ts
@@ -3,18 +3,18 @@ import * as crypto from 'crypto'
 const DEFAULT_ALGORITHM = 'aes-256-ctr'
 
 export class Crypto {
-  public static encrypt (text: string, algorithm = DEFAULT_ALGORITHM): string {
+  public static encrypt (text: string, outputEncoding: crypto.HexBase64BinaryEncoding = 'hex', algorithm = DEFAULT_ALGORITHM): string {
     const cryptoPassword: string = process.env.IREDIUM_CRYPTO_PASSWORD || ''
     var cipher = crypto.createCipher(algorithm, cryptoPassword)
-    var crypted = cipher.update(text, 'utf8', 'hex')
+    var crypted = cipher.update(text, 'utf8', outputEncoding)
     crypted += cipher.final('hex')
     return crypted
   }
 
-  public static decrypt (text: string, algorithm = DEFAULT_ALGORITHM): string {
+  public static decrypt (text: string, inputEncoding: crypto.HexBase64BinaryEncoding = 'hex', algorithm = DEFAULT_ALGORITHM): string {
     const cryptoPassword: string = process.env.IREDIUM_CRYPTO_PASSWORD || ''
     var decipher = crypto.createDecipher(algorithm, cryptoPassword)
-    var dec = decipher.update(text, 'hex', 'utf8')
+    var dec = decipher.update(text, inputEncoding, 'utf8')
     dec += decipher.final('utf8')
     return dec
   }
