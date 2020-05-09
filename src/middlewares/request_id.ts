77ff62cebcab6eca8d655825f73366bfa2a31e77
Wahyu Nugraha <nugraha.c.wahyu@gmail.com>
2020-05-09 12:43:52 +0700

    use res.locals

--- src/middlewares/request_id.ts
+++ src/middlewares/request_id.ts
@@ -5,7 +5,8 @@ import express = require('express')
 export class RequestId extends BaseMiddleware {
   public generate (): express.RequestHandler {
     return async (req, res, next): Promise<void> => {
-      req['request_id'] = req.headers['x-request-id'] || uuid.v4()
+      res.locals.clientTraceId = req.get('x-client-trace-id')
+      res.locals.requestId = req.get('x-request-id') || uuid.v4()
       next()
     }
   }
