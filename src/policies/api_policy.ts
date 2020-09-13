fa9e3ebfcba191f65661726e672cc578daeb48dc
Wahyu Nugraha <nugraha.c.wahyu@gmail.com>
2020-09-14 04:20:06 +0700

    add role checking

--- src/policies/api_policy.ts
+++ src/policies/api_policy.ts
@@ -22,15 +22,27 @@ export class ApiPolicy extends BasePolicy {
   }
 
   public restore (): boolean {
-    return !!this.user
+    return this.isResourceOwner()
   }
 
   public isResourceOwner (): boolean {
     const user = this.user ? this.user : null
     const record = this.record
+    const alwaysAllowedUserRoles = [
+      'root'
+    ]
 
     if (!user) {
       return false
+    }
+
+    if (user.role) {
+      const roles = user.role.split ? user.role.split(' ') : []
+      for (let role of roles) {
+        if (alwaysAllowedUserRoles.includes(role)) {
+          return true
+        }
+      }
     }
 
     return user.id === record.user_id
