bb9a20a6a488989dad694a60d5e7d37f02324724
Wahyu Nugraha <nugraha.c.wahyu@gmail.com>
2020-09-15 23:44:05 +0700

    only protect user api show method

--- src/policies/user_api_policy.ts
+++ src/policies/user_api_policy.ts
@@ -1,10 +1,6 @@
 import { ApiPolicy } from "./api_policy";
 
 export class UserApiPolicy extends ApiPolicy {
-  public index (): boolean {
-    return this.isResourceOwner()
-  }
-
   public show (): boolean {
     return this.isResourceOwner()
   }
